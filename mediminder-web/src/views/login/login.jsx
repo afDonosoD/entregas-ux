b6665439e30dbdab5399393a2068e3e9f0cc15ca
afDonosoD <af.donoso@uniandes.edu.co>
2025-09-20 00:08:37 -0500

    Added all elements to login page

--- mediminder-web/src/views/login/login.jsx
+++ mediminder-web/src/views/login/login.jsx
@@ -1,14 +1,28 @@
 import './login.css'
-import PillsFillIcon from '../../assets/pills.fill.svg'
+import PillsFillIcon from '../../assets/pills.fill.svg?react'
 
 function Login() {
   return (
     <>
-      <div className="container">
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className='d-flex flex-column align-items-center'>
         <h1>Mediminder</h1>
-        <img id='pills-icon' src={PillsFillIcon} alt='Pills Icon' />
-        <p>Please log in to continue</p>
-        <button className="btn btn-primary">Login</button>
+          <PillsFillIcon id='pills-icon' />
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Correo</label>
+              <input type="email" className="form-control" id="email" placeholder='Correo' />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Clave</label>
+              <input type="password" className="form-control" id="password" placeholder='Clave' />
+            </div>
+            <button type="submit" className="btn btn-primary rounded-pill w-100">Iniciar Sesión</button>
+          </form>
+
+          <p>¿No tienes una cuenta? Regístrate</p>
+          <p>Recuperar contraseña</p>
+        </div>
       </div>
     </>
   );
