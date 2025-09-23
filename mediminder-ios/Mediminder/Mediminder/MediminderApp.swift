a52dba65a7cc46666ab3778f090fb8d026b6d933
afDonosoD <af.donoso@uniandes.edu.co>
2025-09-23 16:39:37 -0500

    Created iOS project

---
+++ mediminder-ios/Mediminder/Mediminder/MediminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediminderApp.swift
+//  Mediminder
+//
+//  Created by Andres Donoso on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MediminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
