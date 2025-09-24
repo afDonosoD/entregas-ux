ff0f4aa80c0af023e71c9d442ec4790e6e90ca8b
DavidMS73 <gd.martinez@uniandes.edu.co>
2025-09-23 20:01:51 -0500

    New view

---
+++ mediminder-ios/Mediminder/Mediminder/Views/RemindersView 2.swift
@@ -0,0 +1,49 @@
+//
+//  RemindersView.swift
+//  Mediminder
+//
+//  Created by Andres Donoso on 23/09/25.
+//
+
+import SwiftUI
+
+struct RemindersView2: View {
+	var body: some View {
+		VStack {
+			HStack {
+				Spacer()
+
+				VStack(alignment: .trailing) {
+					Text("Domingo\nSeptiembre 14")
+						.font(.title)
+                        .foregroundStyle(Color(.primary))
+						.bold()
+						.padding(.bottom, 16)
+					
+					Text("¡Buenos días, Mary!")
+						.padding(.bottom, 8)
+					
+					Text("Hoy **si tienes** medicinas pendientes por tomar")
+				}
+				.multilineTextAlignment(.trailing)
+			}
+			.padding(.trailing, 16)
+
+			Spacer()
+		}
+		.background {
+			GeometryReader { proxy in
+				Image(.day)
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width: proxy.size.width / 1.5)
+			}
+			.ignoresSafeArea()
+		}
+	}
+}
+
+#Preview {
+	RemindersView2()
+		.fontDesign(.rounded)
+}
