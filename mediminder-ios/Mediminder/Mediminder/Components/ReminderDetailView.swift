6597cc4e7adbb1af69f678bb53490c03fac52e26
afDonosoD <af.donoso@uniandes.edu.co>
2025-09-24 18:13:58 -0500

    Finish detail view

---
+++ mediminder-ios/Mediminder/Mediminder/Components/ReminderDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderDetailView.swift
+//  Mediminder
+//
+//  Created by Andres Donoso on 24/09/25.
+//
+
+import SwiftUI
+
+struct ReminderDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderDetailView()
+}
