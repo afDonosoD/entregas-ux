6bb8870c4109fef920593e81258a0f94dec497fd
DavidMS73 <gd.martinez@uniandes.edu.co>
2025-09-24 22:02:11 -0500

    Improve

--- mediminder-web/src/App.jsx
+++ mediminder-web/src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 import Login from './views/login/login.jsx'
 import { useLocalStorage } from './hooks/useLocalStorage.js'
@@ -9,7 +7,6 @@ import { useToast } from './hooks/useToast.js'
 import MedicationDetail from './views/medication-detail/medication-detail.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [activeUser, setActiveUser] = useLocalStorage('activeUser', null);
   const { showToast } = useToast();
   let navigate = useNavigate();
@@ -28,26 +25,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <button onClick={handleSignOut}>Sign out</button>
       <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#medication-detail">
         Launch demo modal
